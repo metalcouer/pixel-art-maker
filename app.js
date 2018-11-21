20825a0236e218493c02a483f12e4475e8abaf78
Diane <metalcouer@gmail.com>
2018-11-21 15:26:55 -0700

    added full grid squares to canvas

---
+++ app.js
@@ -0,0 +1,20 @@
+var row = document.querySelector(".row")
+
+
+
+for (i = 1; i <= 3025; i++) {
+
+    var pixels = document.createElement("div")
+    pixels.classList.add("pixels")
+    pixels.addEventListener('click', function('event'){
+        
+
+
+    })
+
+
+
+
+    row.append(pixels)
+    pixels.onclick = 
+}
